ef040daad1132d35532a93e53960796b4e5e8887
Jon Jennemann <github@jonf.xyz>
2025-12-03 13:50:35 +0100

    removed debuglines

--- server.c
+++ server.c
@@ -236,7 +236,6 @@ void *handle_conn(void *arg) {
   printf("client (id:%d) connected\n", connfd);
   document_t *request_document = document_from_stream(connfd);
   if (!request_document->header || !request_document->header->request_line) {
-    printf("ddd\n");
     destroy_document(request_document);
     return NULL;
   }
